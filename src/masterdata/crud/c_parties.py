ab108636931bbbfc46162d0791f8662168cbb994
vbe <vladob87@gmail.com>
2023-10-20 18:21:49 +0000

    working dummy data and fixed error in get c_parties"

--- src/masterdata/crud/c_parties.py
+++ src/masterdata/crud/c_parties.py
@@ -1,4 +1,6 @@
+import logging
 from sqlmodel import Session, select
+from sqlalchemy.exc import SQLAlchemyError
 
 from src.masterdata.models.c_parties import CounterPartiesMain, CounterpartiesAttribs
 
@@ -18,18 +20,21 @@ def insert_cparty(engine, c_party_main: CounterPartiesMain, c_party_atribs: Coun
             session.refresh(atribs_cparty)
             return c_party_main
             # log here if error or return
-        except Exception as e:
-            print(e)
-            return e
+        except SQLAlchemyError as e:
+            logging.error(e)
+            raise e
             # log here if error or return
 
 
 def get_cparties(engine):
-    with Session(engine) as session:
-        statement = select(CounterPartiesMain)
-        results = session.exec(statement)
-        return list(results
-                    )
+    try:
+        with Session(engine) as session:
+            statement = select(CounterPartiesMain)
+            results = session.exec(statement)
+            return results.fetchall()
+    except SQLAlchemyError as e:
+        logging.error(e)
+        return e
 
 
 def get_cparty(engine, id: int):
